ceed1830f76d2601f25ba1d981efb0e83475d20a
GitHub Action <action@github.com>
2022-08-02 00:09:16 +0000

    Update documentation

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("Jhuki 0.1.0-dev7 documentation");
+        $("li.nav-item-0 a").text("Jhuki 0.1.0-dev8 documentation");
     }
 });
