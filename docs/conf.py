06e76e0a075616c9f1c79650c5e851c46ad4b29c
Gabriele Bozzola <sbozzolator@gmail.com>
2022-08-17 12:33:22 -0700

    Bump to 0.1.0-dev9

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2021, Gabriele Bozzola"
 author = "Gabriele Bozzola"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0-dev8"
+release = "0.1.0-dev9"
 
 
 # -- General configuration ---------------------------------------------------
