e7904ae9f05a299196e410322c44b48897a4e15a
Gabriele Bozzola <sbozzolator@gmail.com>
2022-11-18 11:34:01 -0700

    Maintenance: Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "Jhuki"
-copyright = "2021, Gabriele Bozzola"
+copyright = "2021-2022, Gabriele Bozzola"
 author = "Gabriele Bozzola"
 
 # The full version, including alpha/beta/rc tags
